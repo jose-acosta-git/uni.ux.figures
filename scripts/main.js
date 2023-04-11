c80341bd0f1440ec2589f51c40d286d656a48b39
jose-acosta-git <jose.acosta.git@gmail.com>
2023-04-11 09:34:44 -0300

    finish forms generation feature

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,17 @@
 const canvas = document.getElementById('canvas');
-const ctx = canv.getContext('2d');
+const ctx = canvas.getContext('2d');
 
 const canvasWidth = canvas.width;
 const canvasHeight = canvas.height;
 
 const forms = [];
-const forms_max = 10;
-const forms_size = 50
+const forms_max = 30;
+const forms_size = 50;
 
 for (i = 0; i < forms_max; i++) {
     addForm(i < (forms_max / 2));
 }
+draw();
 
 function addForm(style) {
     let posX = Math.round(Math.random() * canvasWidth);
@@ -26,12 +27,19 @@ function addForm(style) {
     }
 }
 
-
-
 function randomRGBA() {
     let r = Math.round(Math.random() * 255);
     let g = Math.round(Math.random() * 255);
     let b = Math.round(Math.random() * 255);
     let a = 255;
     return `rgba(${r}, ${g}, ${b}, ${a})`;
+}
+
+function draw() {
+    const rect = new Rect(0, 0, canvasWidth-1, canvasHeight-1, 'rgba(245, 245, 245, 255)' , ctx, true);
+    rect.draw();
+    
+    for (let i = 0; i < forms_max; i++) {
+        forms[i].draw();
+    }
 }
